8865f3ca2fe50fd57c4455fe71daa318d6fc985c
achyarrbagus <achyarbagus@gmail.com>
2023-06-23 13:29:35 +0700

    db configuration

--- pkg/mysql/mysql.go
+++ pkg/mysql/mysql.go
@@ -19,8 +19,6 @@ func DatabaseInit() {
 	var DB_NAME = os.Getenv("DB_NAME")
 	var DB_PORT = os.Getenv("DB_PORT")
 
-	fmt.Println(DB_HOST)
-
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
