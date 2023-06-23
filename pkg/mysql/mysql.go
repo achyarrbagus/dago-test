b5bc905b7ef9a39c9eda7843597b42770946f23e
achyarrbagus <achyarbagus@gmail.com>
2023-06-23 13:30:05 +0700

    db configuration

--- pkg/mysql/mysql.go
+++ pkg/mysql/mysql.go
@@ -23,11 +23,6 @@ func DatabaseInit() {
 		DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-	// if you want use mysql u can use it
-
-	// dsn := "root:@tcp(localhost:3306)/dago-test?charset=utf8mb4&parseTime=True&loc=Local"
-	// DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
 	if err != nil {
 		panic(err)
 	}
